e2358ae7059e21daccc0594a3603613b8e0e86b3
Varun Vaidya <vvaidya2705@gmail.com>
2023-11-13 18:47:50 -0800

    initial site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,242 @@
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
-const inter = Inter({ subsets: ['latin'] })
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+interface Person {
+  gender: string;
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  location: {
+    street: {
+      number: number;
+      name: string;
+    };
+    city: string;
+    state: string;
+    country: string;
+    postcode: string;
+    coordinates: {
+      latitude: string;
+      longitude: string;
+    };
+    timezone: {
+      offset: string;
+      description: string;
+    };
+  };
+  picture: {
+    medium: string;
+  };
+  dob: {
+    date: string;
+    age: number;
+  };
+  email: string;
+  phone: string;
+  cell: string;
+  nat: string;
+}
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const FetchRandomUsers = () => {
+  const [person, setPerson] = useState<Person[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://randomuser.me/api/?results=10');
+        const data = await response.json();
+        setPerson(data.results);
+      } catch (err) {
+        console.error('Error fetching data: ', err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '20px' }}>
+    {person.map((person, index) => (
+    <div key={index} style={{ border: '1px solid #ccc', padding: '10px' }}>
+    <Box sx={{ width: '100%' }}>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={12}>
+          <Item>
+            <Avatar>
+                <AvatarImage src={person.picture.medium} alt={`${person.name.first} ${person.name.last}`} style={{ width: '50%' }} />
+            </Avatar>
+          <Dialog>
+            <DialogTrigger>
+            {`${person.name.title} ${person.name.first} ${person.name.last}`}
+            </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Want to learn more about this person?</DialogTitle>
+                  <DialogDescription>
+                    {`${person.name.title} ${person.name.first} ${person.name.last}`}
+                    <br></br>
+                    {person.email}
+                    <br></br>
+                    {`${person.location.street.number} ${person.location.street.name} ${person.location.city} ${person.location.state} ${person.location.country}`}
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog> 
+           <p>Email: {person.email}</p>
+           <p>Phone: {person.phone}</p>
+           <p>Age: {person.dob.age}</p>
+          </Item>
+        </Grid>
+      </Grid>
+    </Box>
+    </div>
+  ))}
+  </div>
+    // <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '20px' }}>
+    //   {person.map((person, index) => (
+    //     <div key={index} style={{ border: '1px solid #ccc', padding: '10px' }}>
+    //       <img src={person.picture.medium} alt={`${person.name.first} ${person.name.last}`} style={{ width: '50%' }} />
+    //       <h3>{`${person.name.title} ${person.name.first} ${person.name.last}`}</h3>
+    //       <p>Email: {person.email}</p>
+    //       <p>Phone: {person.phone}</p>
+    //       <p>Age: {person.dob.age}</p>
+    //     </div>
+    //   ))}
+    // </div>
+  );
+};
+
+export default FetchRandomUsers;
+
+/*
+import internal from "stream";
+
+async function fetchData() {
+  const response = await fetch('https://randomuser.me/api/?results=10')
+  const users = await response.json()
+  return users
+}
 
 export default function Home() {
+  const people = fetchData()
+  console.log(people)
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+    
+    </>
   )
 }
+
+type ResultsArray = {
+  results: Result[],
+  info: Info
+};
+
+interface Result {
+  gender: string,
+  name: Name,
+  location: Location,
+  email: string,
+  login: Login,
+  dob: DOB,
+  registered: Registered,
+  phone: string,
+  cell: string,
+  id: ID,
+  picture: Picture,
+  nat: string
+}
+
+interface Name {
+  title: string,
+  first: string,
+  last: string
+}
+
+interface Street {
+  number: number,
+  name: string
+}
+
+interface Location {
+  street: Street,
+  city: string,
+  state: string,
+  country: string,
+  postcode: string,
+  coordinates: Coordinates,
+  timezone: Timezone
+}
+
+interface Coordinates {
+  latitude: string,
+  longitude: string
+}
+
+interface Timezone {
+  offset: string,
+  description: string
+}
+
+interface Login {
+  uuid: string,
+  username: string,
+  password: string,
+  salt: string,
+  md5: string,
+  sha1: string,
+  sha256: string
+}
+
+interface DOB {
+  date: string,
+  age: number
+}
+
+interface Registered {
+  date: string,
+  age: number
+}
+
+interface ID {
+  name: string,
+  value: string
+}
+
+interface Picture {
+  large: string,
+  medium: string,
+  thumbnail: string
+}
+
+interface Info {
+  seed: string,
+  results: number,
+  page: number,
+  version: string
+}
+*/
